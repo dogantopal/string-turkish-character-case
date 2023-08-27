b321f80a47a0b8435d79aa253f213c4ad6af6f76
Doğan Can Topal <dogancan.topal@ciceksepeti.com>
2023-08-27 17:39:50 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+Thread.CurrentThread.CurrentCulture = new CultureInfo("tr-TR");
+
+const string turkishWord = "istikbal";
+
+Console.WriteLine(turkishWord.ToUpper() == "ISTIKBAL");     //Return false
+
+Console.WriteLine(turkishWord.ToUpper(new CultureInfo("tr-TR")) == "ISTIKBAL");     //Return false
+
+Console.WriteLine(string.Equals(turkishWord, "ISTIKBAL", StringComparison.CurrentCultureIgnoreCase));      //Return false
+
+Console.WriteLine(string.Equals(turkishWord, "ISTIKBAL", StringComparison.OrdinalIgnoreCase));      //Return true
+
+Console.WriteLine(string.Equals(turkishWord, "ISTIKBAL", StringComparison.InvariantCultureIgnoreCase));      //Return true
